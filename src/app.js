5497cb5cc431cd8949838bbb34edae909247656b
reetjaspinder <reetjaspinder@gmail.com>
2024-08-15 19:06:12 +0530

    Test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const fs = require('fs');
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
 
 const STATUS_USER_ERROR = 422;
 
@@ -10,10 +10,10 @@ server.use(bodyParser.json());
 
 /* Returns a list of dictionary words from the words.txt file. */
 const readWords = () => {
-  const contents = fs.readFileSync('words.txt', 'utf8');
-  return contents.split('\n');
+  const contents = fs.readFileSync("words.txt", "utf8");
+  return contents.split("\n");
 };
 
-// TODO: your code to handle requests
+// TODO: your code to handle
 
 server.listen(3000);
