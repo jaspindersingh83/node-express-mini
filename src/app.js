7e898ff8260e35f318e6fa18c44071892f4e910d
jaspindersingh83 <jaspindersingh83@gmail.com>
2024-08-16 15:24:35 +0530

    Complete

--- src/app.js
+++ src/app.js
@@ -14,6 +14,51 @@ const readWords = () => {
   return contents.split("\n");
 };
 
+const allWords = readWords();
+const randomInt = Math.floor(Math.random() * allWords.length);
+const finalWorld = allWords[randomInt].toLowerCase();
+console.log(finalWorld);
+let lettersSoFar = new Set();
+
+server.get("/", (req, res) => {
+  res.status(200).send("Hello the Bollywood Star");
+});
 // TODO: your code to handle
+
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i);
+}
+server.post("/guess", (req, res) => {
+  let { letter } = req.body;
+  if (!isLetter(letter)) {
+    res.status(400).send({ error: "Kindly enter an alphabet" });
+  } else {
+    if (lettersSoFar.has(letter)) {
+      res.status(400).send({ error: "Letter Already Guessed" });
+    } else {
+      lettersSoFar.add(letter);
+      res.send({ success: "letter added to guess" });
+    }
+  }
+});
+
+server.get("/guess", (req, res) => {
+  let wordSoFar = "";
+  let flag = true;
+  for (let i = 0; i < finalWorld.length; i++) {
+    let ch = finalWorld.charAt(i);
+    if (lettersSoFar.has(ch)) {
+      wordSoFar += ch;
+    } else {
+      flag = false;
+      wordSoFar += "-";
+    }
+  }
+  if (flag) {
+    res.status(200).send(`Hurray you guesses the complete word ${wordSoFar}`);
+  } else {
+    res.status(200).send({ wordSoFar, guesses: Array.from(lettersSoFar) });
+  }
+});
 
 server.listen(3000);
